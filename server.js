0dd84dd0467b23782cd419e80fb54387841ffac6
charlenne alexander <charlennep@gmail.com>
2020-04-20 15:12:51 -0500

    doing sme styling...

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const apiRoutes = require("./routes/api");
 const htmlRoutes = require("./routes/html");
-const cfonts = require("cfonts");
 
 // parser middlewear
 app.use(express.json());
